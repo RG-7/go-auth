924aaf416a453d3bfdf1666f1f65efd4d50ac57a
Ratn Govindam <rg.ratn@gmail.com>
2025-02-18 23:22:00 +0530

    add server and port to run

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/RG-7/go-auth/config"
+	"github.com/RG-7/go-auth/helpers"
+	"github.com/RG-7/go-auth/routes"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	Key := config.GenerateRandomKey()
+	helpers.SetJWTKey(Key)
+
+	r := gin.Default()
+
+	routes.SetupRoutes(r)
+
+	// start the server
+	r.Run(":8080")
+	log.Println("🚀Server is running on port: 8080")
+}
